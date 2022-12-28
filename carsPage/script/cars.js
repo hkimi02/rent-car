8799de1ff08bf43a513f10a01c4928c80f40ef32
hkimi amin <hkimiamin02@gmail.com>
2022-12-28 23:49:48 +0100

    update cars affuchage from js

--- carsPage/script/cars.js
+++ carsPage/script/cars.js
@@ -1,5 +1,5 @@
 let cars = [{
-        name: "BMW",
+        name: "audi",
         model: "X5",
         year: 2010,
         price: 50000,
@@ -7,7 +7,14 @@ let cars = [{
         photo: '../assets/x5_car.jpg'
     },
     {
-        name: "BMW",
+        name: "punto",
+        model: "X5",
+        year: 2010,
+        price: 50000,
+        rented: false,
+        photo: '../assets/x5_car.jpg'
+    }, {
+        name: "fox",
         model: "X5",
         year: 2010,
         price: 50000,
@@ -53,16 +60,28 @@ let cars = [{
         model: "X5",
         year: 2010,
         price: 50000,
-        rented: false,
-        photo: '../assets/x5_car.jpg'
-    }, {
-        name: "BMW",
-        model: "X5",
-        year: 2010,
-        price: 50000,
-        rented: false,
+        rented: true,
         photo: '../assets/x5_car.jpg'
     },
 ]
 let space = document.getElementById('cars');
-space.innerHTML += ''
+let length = cars.length;
+let i = 0;
+for (i = 0; i < length; i++) {
+    space.innerHTML += `
+    <div class="car">
+    <div class="car-image">
+        <img src="${cars[i].photo}" alt="" width="300px" width="300px">
+    </div>
+    <div class="card-body">
+        <h3>${cars[i].name + ' '+cars[i].model }</h3>
+        <h3>${cars[i].price}</h3>
+        <h3>${cars[i].year}</h3>
+        <h3>${cars[i].rented==false ? 'not rented' : 'rented'}</h3>
+    </div>
+    <div class="card-footer">
+        <button class="btn-rent">RENT</button>
+    </div>
+</div>
+    `;
+}
