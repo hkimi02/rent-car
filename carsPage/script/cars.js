91ab3cf1d78b0ca1fe78c6d96fc692f2b3c263e3
hkimi amin <hkimiamin02@gmail.com>
2022-12-30 18:19:06 +0100

    add funtion rent update css

--- carsPage/script/cars.js
+++ carsPage/script/cars.js
@@ -64,6 +64,23 @@ let cars = [{
         photo: '../assets/x5_car.jpg'
     },
 ]
+
+function rent() {
+    let btns = document.querySelectorAll('.btn-rent');
+    btns.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            if (cars[index].rented == false) {
+                cars[index].rented = true;
+                btn.innerHTML = 'RENTED';
+                btn.style.backgroundColor = 'red';
+            } else {
+                cars[index].rented = false;
+                btn.innerHTML = 'RENT';
+                btn.style.backgroundColor = 'green';
+            }
+        })
+    })
+}
 let space = document.getElementById('cars');
 let length = cars.length;
 let i = 0;
@@ -74,6 +91,7 @@ for (i = 0; i < length; i++) {
         <img src="${cars[i].photo}" alt="" width="300px" width="300px">
     </div>
     <div class="card-body">
+        <h3>${"car number : " + (i+1)}</h3>
         <h3>${cars[i].name + ' '+cars[i].model }</h3>
         <h3>${cars[i].price + ' TND'}</h3>
         <h3>${cars[i].year}</h3>
@@ -85,3 +103,4 @@ for (i = 0; i < length; i++) {
 </div>
     `;
 }
+rent();
