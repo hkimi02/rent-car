0f6c2c73da42124c9605776ab9774a82c599e9c5
hkimi amin <hkimiamin02@gmail.com>
2022-12-30 23:10:24 +0100

    .. update structure

--- reclamationPage/script/main.js
+++ reclamationPage/script/main.js
@@ -1,11 +1,12 @@
 function valideReclamation() {
+    let erreur = document.getElementById("erreur");
     let input_group = document.getElementsByClassName("input-form");
     console.log(input_group);
     let i = 0;
     for (i = 0; i < input_group.length; i++) {
         if (input_group[i].value == "") {
             input_group[i].style.border = "2px solid red";
-            alert("please fll all the fields");
+            erreur.innerHTML = "please fill all the fields";
             return false;
         }
     }
